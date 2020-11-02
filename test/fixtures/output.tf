965107ad86c706f246cb7e6cec93daa201944b43
Slav Donchev <slav.donchev@gmail.com>
2020-11-02 13:47:30 +0200

    add module otput for the load balancer ARN.

--- test/fixtures/output.tf
+++ test/fixtures/output.tf
@@ -3,6 +3,12 @@ output "lb_dns_name" {
   description = "DNS name of the created load balancer."
 }
 
+output "lb_arn" {
+  value       = module.tfe_network_lb.lb_arn
+  description = "The ARN of the created load balancer."
+}
+
+
 output "lb_tg_80_arn" {
   value       = module.tfe_network_lb.lb_tg_80_arn
   description = "ARN (id) of the load balancer target group assigned to the listner on port 80."
@@ -16,4 +22,19 @@ output "lb_tg_443_arn" {
 output "lb_tg_8800_arn" {
   value       = module.tfe_network_lb.lb_tg_8800_arn
   description = "ARN (id) of the load balancer target group assigned to the listner on port 8800."
+}
+
+output "lb_tg_80_full" {
+  value       = data.aws_lb_target_group.lb_tg_80
+  description = "The full Target Group object for TG on port 80."
+}
+
+output "lb_tg_443_full" {
+  value       = data.aws_lb_target_group.lb_tg_443
+  description = "The full Target Group object for TG on port 443."
+}
+
+output "lb_tg_8800_full" {
+  value       = data.aws_lb_target_group.lb_tg_8800
+  description = "The full Target Group object for TG on port 8800."
 }
